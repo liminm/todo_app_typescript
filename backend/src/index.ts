9ecf5a5a02e7e76c6e78e866d5817a232e6e3f26
liminm <liminmalek@gmail.com>
2025-02-15 14:35:15 +0100

    Move out server into app.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,4 @@
-import express, {Application, Request, Response} from 'express';
-import todosRouter from './routes/todos';
-import cors from 'cors'; // Import the cors package
-
-const app: Application = express();
-app.use(cors());
-
-app.use(express.json());
-
-app.get('/api/health', (req: Request, res: Response) => {
-    res.send(200).json({status: 'OK'})
-})
-
-app.use('/api/todos', todosRouter);
-
+import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
