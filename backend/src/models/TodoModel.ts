ea9cd2911c005f54f26e519445d352a8b939b993
liminm <liminmalek@gmail.com>
2025-02-19 23:20:31 +0100

    Add user attribute to database model

--- backend/src/models/TodoModel.ts
+++ backend/src/models/TodoModel.ts
@@ -3,11 +3,13 @@ import mongoose, {Schema, Document} from 'mongoose';
 export interface ITodo extends Document {
     title: string
     completed: boolean
+    user: mongoose.Schema.Types.ObjectId
 }
 
 const TodoSchema: Schema = new Schema({
     title: {type: String, required: true},
-    completed: {type: Boolean, default: false}
+    completed: {type: Boolean, default: false},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
 })
 
 export default mongoose.model<ITodo>('Todo', TodoSchema)
